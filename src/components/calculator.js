7fec7b20de4ae034af669fa0eaadcb322a126a44
Rajath Shashidhar Bharadwaj <rj839212@dal.ca>
2022-07-02 23:37:50 -0300

    calculator

---
+++ src/components/calculator.js
@@ -0,0 +1,54 @@
+import React, { useState } from 'react'
+import './calculator.css'
+
+function Calculator() {
+
+    const [result, setResult] = useState('')
+
+    const handleClick = (e) => {
+        setResult(result.concat(e.target.name))
+    }
+
+    const clear = () => {
+        setResult('')
+    }
+    const del = () => {
+        setResult(result.slice(0, -1))
+    }
+
+    const calculate = () => {
+        try {
+            setResult(eval(result).toString())
+        } catch (error) {
+            setResult("Error")
+        }
+    }
+
+  return (
+    <div className='calculator-grid'>
+        <div className='output'>
+           <div className='operand'>{result}</div>
+        </div>
+        <button id="clear" onClick={clear} className='span-two'>AC</button>
+        <button id="del" onClick={del}>DEL</button>
+        <button name="/" onClick={handleClick}>÷</button>
+        <button name="7" onClick={handleClick}>7</button>
+        <button name="8" onClick={handleClick}>8</button>
+        <button name="9" onClick={handleClick}>9</button>
+        <button name="*" onClick={handleClick}>x</button>
+        <button name="4" onClick={handleClick}>4</button>
+        <button name="5" onClick={handleClick}>5</button>
+        <button name="6" onClick={handleClick}>6</button>
+        <button name="+" onClick={handleClick}>+</button>
+        <button name="1" onClick={handleClick}>1</button>
+        <button name="2" onClick={handleClick}>2</button>
+        <button name="3" onClick={handleClick}>3</button>
+        <button name="-" onClick={handleClick}>-</button>
+        <button name="." onClick={handleClick}>.</button>
+        <button name="0" onClick={handleClick}>0</button>
+        <button onClick={calculate} className='span-two'>=</button>
+    </div>
+  )
+}
+
+export default Calculator
